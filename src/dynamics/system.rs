03f8431c0237f14e6385023d8f0b3af0f65cc045
Sanjay Swain <sanjayswain450@gmail.com>
2026-03-25 15:36:15 +0530

    Partial implementation of World structure and body creation

--- src/dynamics/system.rs
+++ src/dynamics/system.rs
@@ -1,19 +1,36 @@
-use nalgebra as nl;
+use nalgebra as na;
 
 pub struct State {
-    position: nl::Vector3<f64>,
-    velocity: nl::Vector3<f64>,
-    quaternion: nl::UnitQuaternion<f64>,
-    angular_velocity: nl::Vector3<f64>,
+    position: na::Vector3<f64>,
+    velocity: na::Vector3<f64>,
+    quaternion: na::UnitQuaternion<f64>,
+    angular_velocity: na::Vector3<f64>,
 }
 
 pub struct Body {
     mass: f64,
-    inertia: nl::Matrix3<f64>,
+    inertia: na::Matrix3<f64>,
     state: State,
 }
 
 pub struct World {
     bodies: Vec<Body>,
     enable_gravity: bool,
+    gravity: f64,
+}
+
+impl World {
+    fn create_body(mass: f64, inertia: na::Matrix3<f64>) -> Body {
+        // TODO: Needs to be updated to automatically append created bodies to world instance
+        Body {
+            mass: mass,
+            inertia: inertia,
+            state: State {
+                position: na::Vector3::<f64>::zeros(),
+                velocity: na::Vector3::<f64>::zeros(),
+                quaternion: na::UnitQuaternion::identity(),
+                angular_velocity: na::Vector3::<f64>::zeros(),
+            },
+        }
+    }
 }
