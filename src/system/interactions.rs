3bb1046afa6b1218424693711091259e176f54a9
Sanjay Swain <sanjayswain450@gmail.com>
2026-03-28 14:53:45 +0530

    Implement some constants for Force and Torque and implement a method to change the frame from global to local and vice versa to both the structs

--- src/system/interactions.rs
+++ src/system/interactions.rs
@@ -1,4 +1,4 @@
-use glam::DVec3;
+use glam::{DQuat, DVec3};
 
 /// Specifies the reference frame of a vector
 #[derive(Clone, Copy)]
@@ -19,12 +19,38 @@ pub struct Force {
 }
 
 impl Force {
+    pub const ZERO_LOCAL: Self = Self {
+        force: DVec3::ZERO,
+        position: DVec3::ZERO,
+        frame: Frame::Local,
+    };
+
+    pub const ZERO_GLOBAL: Self = Self {
+        force: DVec3::ZERO,
+        position: DVec3::ZERO,
+        frame: Frame::Global,
+    };
+
     pub fn new(force: DVec3, position: DVec3, frame: Frame) -> Self {
         Self {
             force: force,
             position: position,
             frame: frame,
         }
+    }
+
+    pub fn to_global(&self, orientation: DQuat) -> DVec3 {
+        return match self.frame {
+            Frame::Global => self.force,
+            Frame::Local => orientation * self.force,
+        };
+    }
+
+    pub fn to_local(&self, orientation: DQuat) -> DVec3 {
+        return match self.frame {
+            Frame::Global => orientation.inverse() * self.force,
+            Frame::Local => self.force,
+        };
     }
 }
 
@@ -35,10 +61,34 @@ pub struct Torque {
 }
 
 impl Torque {
+    pub const ZERO_LOCAL: Self = Self {
+        torque: DVec3::ZERO,
+        frame: Frame::Local,
+    };
+
+    pub const ZERO_GLOBAL: Self = Self {
+        torque: DVec3::ZERO,
+        frame: Frame::Global,
+    };
+
     pub fn new(torque: DVec3, frame: Frame) -> Self {
         Self {
             torque: torque,
             frame: frame,
+        }
+    }
+
+    pub fn to_global(&self, orientation: DQuat) -> DVec3 {
+        match self.frame {
+            Frame::Global => self.torque,
+            Frame::Local => orientation * self.torque,
+        }
+    }
+
+    pub fn to_local(&self, orientation: DQuat) -> DVec3 {
+        match self.frame {
+            Frame::Global => orientation.inverse() * self.torque,
+            Frame::Local => self.torque,
         }
     }
 }
