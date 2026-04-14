9cb82038c80c5b20ff139d00e06b7ac078536e69
Sanjay Swain <sanjayswain450@gmail.com>
2026-04-14 15:44:16 +0530

    Change Jacobian to JacobianRow

--- src/system/joints.rs
+++ src/system/joints.rs
@@ -2,7 +2,7 @@ use glam::DVec3;
 
 use crate::system::state::State;
 
-pub struct Jacobian {
+pub struct JacobianRow {
     pub v_a: DVec3,
     pub w_a: DVec3,
     pub v_b: DVec3,
@@ -10,5 +10,10 @@ pub struct Jacobian {
 }
 
 pub trait Joint {
-    fn calculate_jacobian(state_a: State, state_b: State) -> Jacobian;
+    fn calculate_jacobian(
+        state_a: State,
+        state_b: State,
+        anchor_a: DVec3,
+        anchor_b: DVec3,
+    ) -> Vec<JacobianRow>;
 }
