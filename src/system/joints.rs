cefa0250f2ee644691d97870321f2ebd6b5f847e
Sanjay Swain <sanjayswain450@gmail.com>
2026-04-14 10:21:58 +0530

    Update trait definition

--- src/system/joints.rs
+++ src/system/joints.rs
@@ -1,5 +1,7 @@
 use glam::DVec3;
 
+use crate::system::state::State;
+
 pub struct Jacobian {
     pub v_a: DVec3,
     pub w_a: DVec3,
@@ -8,5 +10,5 @@ pub struct Jacobian {
 }
 
 pub trait Joint {
-    fn calculate_jacobian() -> Jacobian;
+    fn calculate_jacobian(state_a: State, state_b: State) -> Jacobian;
 }
