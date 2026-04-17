6f42238dcf652e954f33b37b14c7073e2f59dae8
Sanjay Swain <sanjayswain450@gmail.com>
2026-04-17 13:47:36 +0530

    Add tests for dot product of JacobianRow

--- src/system/constraints/joints.rs
+++ src/system/constraints/joints.rs
@@ -47,3 +47,37 @@ pub trait Joint {
         velocity_bias: &mut [f64; 6],
     );
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn dot_product_tests() {
+        let zero_jacobian = JacobianRow::ZERO;
+        let ones_jacobian = JacobianRow {
+            v_a: DVec3::ONE,
+            w_a: DVec3::ONE,
+            v_b: DVec3::ONE,
+            w_b: DVec3::ONE,
+        };
+        let complex_jacobian_1 = JacobianRow {
+            v_a: DVec3::new(1.0, -2.0, 3.0),
+            w_a: DVec3::new(0.0, 5.0, -1.0),
+            v_b: DVec3::new(0.5, 2.0, -3.0),
+            w_b: DVec3::new(1.0, 0.0, -4.0),
+        };
+
+        let complex_jacobian_2 = JacobianRow {
+            v_a: DVec3::new(2.0, 3.0, -1.0),
+            w_a: DVec3::new(5.0, 0.0, 4.0),
+            v_b: DVec3::new(2.0, -1.0, 0.0),
+            w_b: DVec3::new(3.0, 6.0, 1.0),
+        };
+
+        assert!((zero_jacobian.dot(&zero_jacobian)).abs() < 1e-6);
+        assert!((ones_jacobian.dot(&zero_jacobian)).abs() < 1e-6);
+        assert!((ones_jacobian.dot(&ones_jacobian) - 12.0).abs() < 1e-6);
+        assert!((complex_jacobian_1.dot(&complex_jacobian_2) - (-13.0)).abs() < 1e-6);
+    }
+}
