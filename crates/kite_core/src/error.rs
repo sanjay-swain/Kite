e7990a5fc1d720ecc7260858fd58c8b8b580ba11
Sanjay Swain <sanjayswain450@gmail.com>
2026-07-29 14:28:10 +0530

    Add proper Error descriptions

--- crates/kite_core/src/error.rs
+++ crates/kite_core/src/error.rs
@@ -1,7 +1,29 @@
 use glam::DMat3;
+use std::fmt::{self};
 
+#[derive(Debug)]
 pub enum PhysicsError {
     InvalidMass(f64),
     InvalidInertia(DMat3),
     InvalidStepSize(f64),
 }
+
+impl fmt::Display for PhysicsError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            PhysicsError::InvalidInertia(_) => {
+                write!(f, "The inertia matrix is invalid")
+            }
+
+            PhysicsError::InvalidMass(_) => {
+                write!(f, "The mass can't be negative or zero")
+            }
+
+            PhysicsError::InvalidStepSize(_) => {
+                write!(f, "The integrator step size must be positive")
+            }
+        }
+    }
+}
+
+impl std::error::Error for PhysicsError {}
